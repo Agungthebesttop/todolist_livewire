bf83bca1dfce76cc01214f6ad3181b9770a9f52a
Agungthebesttop <agungthebesttop@gmail.com>
2022-03-24 16:12:57 +0700

    todolist
    v1

---
+++ resources/views/livewire/add-todos.blade.php
@@ -0,0 +1,8 @@
+<div class="flex flex-col items-center mt-4">
+    <input type="text" placeholder=" Write your task" wire:model="item" wire:keydown.enter="addTodo">
+   
+    @error('item')
+    <span class="text-red-500 mt-2">{{ $message }}</span>
+    @enderror
+</div>
+
